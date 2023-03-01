20f1eab1ff478ec50a2deae53bc4cf1a288ad382
Priyansh Saxena <priyanshsaxena@Priyanshs-MacBook-Air.local>
2023-03-01 14:02:48 +0530

    Coded returning message

--- example.py
+++ example.py
@@ -1,13 +1,16 @@
 import json
-function = [obj1 := {"func" : "+", "description" : 100,"value" : 542.23},
-            obj2 := {"func" : "-", "description" : -100,"value" : 5124},] 
+function = [obj1 := {"func" : "+", "description" : "Add"},
+            obj2 := {"func" : "-", "description" : "Subtract"}] 
 
 x = input("Enter char :")
 
 for i in range(0, len(function)):
     if x == function[i]["func"]:
         with open('data.json', 'w') as fp:
-            json.dump(function, fp)
+            json.dump(function[i], fp)
         
+f = open('data.json', 'r')
+dat = json.loads(f.read())
 
-
+print(dat["description"])    
+f.close()
